826e7451fb2a153abc03d22ca26d38ac2b2a7ed5
henrinest <joaohswanderley@gmail.com>
2025-08-26 19:22:18 -0300

    Treino com Arrays.

--- src/main.js
+++ src/main.js
@@ -12,17 +12,35 @@ function pergunta(questao) {
     }
 )}
 
-async function funcIdade() {
+async function exArray() {
 
-    let pessoa = {
-        nome : "Ana",
-        idade: 25,
-        cidade: "São Paulo"
-    };
+    let numerosInt = []
 
-    for (chave in pessoa) {
-        console.log(`${chave}: ${pessoa [chave]}`);
+    const num1 = await pergunta("Digite um número inteiro: ")
+    numerosInt[0] = parseFloat(num1)
+
+    const num2 = await pergunta("Digite um número inteiro: ")
+    numerosInt[1] = parseFloat(num2)
+
+    const num3 = await pergunta("Digite um número inteiro: ")
+    numerosInt[2] = parseFloat(num3)
+
+    const num4 = await pergunta("Digite um número inteiro: ")
+    numerosInt[3] = parseFloat(num4)
+
+    const num5 = await pergunta("Digite um número inteiro: ")
+    numerosInt[4] = parseFloat(num5)
+
+    if (Number.isInteger(numerosInt[0]) && (numerosInt[1]) && (numerosInt[2]) && (numerosInt[3]) && (numerosInt[4]) ) {
+        console.log (`Array com 5 números inteiros: ${numerosInt} `)
+    } else {
+        console.log("Reset")
+        console.log (`Array com 5 números inteiros: ${numerosInt} `)
+        console.log
+        numerosInt = []
+        return exArray()
     }
+
 }
 
-funcIdade()
+exArray()
