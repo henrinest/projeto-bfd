29db81b24f00bf2efb5d5d76b288ae05a46133ef
henrinest <joaohswanderley@gmail.com>
2025-08-26 20:11:21 -0300

    Treino das medalhas.

--- src/main.js
+++ src/main.js
@@ -12,35 +12,18 @@ function pergunta(questao) {
     }
 )}
 
-async function exArray() {
+async function medalhaDeAcordoComPosicao() {
+    let medalhas = ["Ouro", "Prata", "Bronze", "NADA"]
 
-    let numerosInt = []
+    const posCorrida = await pergunta ("Digite a sua posição na corrida: ")
+    const num = parseInt(posCorrida) - 1
 
-    const num1 = await pergunta("Digite um número inteiro: ")
-    numerosInt[0] = parseFloat(num1)
-
-    const num2 = await pergunta("Digite um número inteiro: ")
-    numerosInt[1] = parseFloat(num2)
-
-    const num3 = await pergunta("Digite um número inteiro: ")
-    numerosInt[2] = parseFloat(num3)
-
-    const num4 = await pergunta("Digite um número inteiro: ")
-    numerosInt[3] = parseFloat(num4)
-
-    const num5 = await pergunta("Digite um número inteiro: ")
-    numerosInt[4] = parseFloat(num5)
-
-    if (Number.isInteger(numerosInt[0]) && (numerosInt[1]) && (numerosInt[2]) && (numerosInt[3]) && (numerosInt[4]) ) {
-        console.log (`Array com 5 números inteiros: ${numerosInt} `)
+    if (num >= 0 && num <= 2){
+    console.log (`Sua medalha reccebida foi: ${medalhas[num]}`)
     } else {
-        console.log("Reset")
-        console.log (`Array com 5 números inteiros: ${numerosInt} `)
-        console.log
-        numerosInt = []
-        return exArray()
+    console.log (`Sua medalha reccebida foi: ${medalhas[3]}`)
     }
-
+    rl.close()
 }
 
-exArray()
+medalhaDeAcordoComPosicao()
