9289566b8c259d8675b68f9ed6957d2a56210f9a
eversondamiao <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>
2025-08-26 23:28:18 -0300

    calculadora: média de notas concluída (+)

--- src/main.js
+++ src/main.js
@@ -1,56 +1,45 @@
-const readline = require("node:readline");
+const rl = require("node:readline");
 
+const prompt = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-function perguntar(pergunta) {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
+prompt.question("Qual a sua primeira nota: ", escreva1 => {
+    prompt.question("Qual a sua segunda nota: ", escreva2 => {
+        prompt.question("Qual a sua terceira nota: ", escreva3 => {
+            const nota1 = Number.parseFloat(escreva1);
+            const nota2 = Number.parseFloat(escreva2);
+            const nota3 = Number.parseFloat(escreva3);
 
-    return new Promise(resolve => {
-        rl.question(pergunta, resposta => {
-            rl.close();
-            resolve(resposta);
+            
+            if (
+                Number.isNaN(nota1) || Number.isNaN(nota2) || Number.isNaN(nota3)
+            ) {
+                console.log("Um dos campos não foi preenchido corretamente. Tente novamente.");
+                console.log("Por favor, insira apenas números válidos de 0 a 10.");
+            } else if (
+                nota1 > 10 || nota2 > 10 || nota3 > 10
+            ) {
+                console.log("Por favor, insira apenas números válidos.");
+                console.log("Preencha com números de 0 a 10.");
+            } else if (
+                nota1 < 0 || nota2 < 0 || nota3 < 0
+            ) {
+                console.log("O valor deve ser maior ou igual a 0.");
+            } else {
+                const media = (nota1 + nota2 + nota3) / 3;
+
+                if (media <= 6) {
+                    console.log(`Sua média é ${media.toFixed(2)} e você foi REPROVADO!`);
+                } else if (media <= 8) {
+                    console.log(`Sua média é ${media.toFixed(2)} e você foi aprovado na MÉDIA!`);
+                } else {
+                    console.log(`Sua média é ${media.toFixed(2)} e você foi APROVADO!`);
+                }
+            }
+
+            prompt.close();
         });
     });
-}
-
-
-async function calcularMedia() {
-    const resposta1 = await perguntar("Qual a sua primeira nota: ");
-    const resposta2 = await perguntar("Qual a sua segunda nota: ");
-    const resposta3 = await perguntar("Qual a sua terceira nota: ");
-
-    const nota1 = parseFloat(resposta1);
-    const nota2 = parseFloat(resposta2);
-    const nota3 = parseFloat(resposta3);
-
-    
-    if (Number.isNaN(nota1) || Number.isNaN(nota2) || Number.isNaN(nota3)) {
-        console.log("Um dos campos não foi preenchido corretamente.");
-        console.log("Por favor, insira apenas números válidos de 0 a 10.");
-        return;
-    }
-
-    if (nota1 < 0 || nota2 < 0 || nota3 < 0) {
-        console.log("As notas devem ser maiores ou iguais a 0.");
-        return;
-    }
-
-    if (nota1 > 10 || nota2 > 10 || nota3 > 10) {
-        console.log("As notas devem ser menores ou iguais a 10.");
-        return;
-    }
-
-    const media = (nota1 + nota2 + nota3) / 3;
-
-    if (media <= 6) {
-        console.log(`Sua média é ${media.toFixed(2)} e você foi REPROVADO!`);
-    } else if (media <= 8) {
-        console.log(`Sua média é ${media.toFixed(2)} e você foi aprovado na MÉDIA!`);
-    } else {
-        console.log(`Sua média é ${media.toFixed(2)} e você foi APROVADO!`);
-    }
-}
-
-calcularMedia();
+});
