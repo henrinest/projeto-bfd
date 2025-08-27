f6f35cee15305a06b9ca0ee892f084daea2aa712
eversondamiao <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>
2025-08-26 23:21:09 -0300

    calculadora: média de notas concluída

--- src/main.js
+++ src/main.js
@@ -1,19 +1,56 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
+const readline = require("node:readline");
 
-/// Programa inicial, do projeto BFD.
 
-app.get('/', (req, res) => {
-    console.log("Requisição realizada");
-    res.send("Hello World! Estou no GET");
-})
+function perguntar(pergunta) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
 
-app.post('/', (req, res) => {
-    console.log("Requisição realizada / Entrei no POST");
-    res.send("Hello World! Estou no POST");
-})
+    return new Promise(resolve => {
+        rl.question(pergunta, resposta => {
+            rl.close();
+            resolve(resposta);
+        });
+    });
+}
 
-app.listen(PORT, () => {
-    console.log(`Olá, mundo! App online na porta: ${PORT}`);
-})
+
+async function calcularMedia() {
+    const resposta1 = await perguntar("Qual a sua primeira nota: ");
+    const resposta2 = await perguntar("Qual a sua segunda nota: ");
+    const resposta3 = await perguntar("Qual a sua terceira nota: ");
+
+    const nota1 = parseFloat(resposta1);
+    const nota2 = parseFloat(resposta2);
+    const nota3 = parseFloat(resposta3);
+
+    
+    if (Number.isNaN(nota1) || Number.isNaN(nota2) || Number.isNaN(nota3)) {
+        console.log("Um dos campos não foi preenchido corretamente.");
+        console.log("Por favor, insira apenas números válidos de 0 a 10.");
+        return;
+    }
+
+    if (nota1 < 0 || nota2 < 0 || nota3 < 0) {
+        console.log("As notas devem ser maiores ou iguais a 0.");
+        return;
+    }
+
+    if (nota1 > 10 || nota2 > 10 || nota3 > 10) {
+        console.log("As notas devem ser menores ou iguais a 10.");
+        return;
+    }
+
+    const media = (nota1 + nota2 + nota3) / 3;
+
+    if (media <= 6) {
+        console.log(`Sua média é ${media.toFixed(2)} e você foi REPROVADO!`);
+    } else if (media <= 8) {
+        console.log(`Sua média é ${media.toFixed(2)} e você foi aprovado na MÉDIA!`);
+    } else {
+        console.log(`Sua média é ${media.toFixed(2)} e você foi APROVADO!`);
+    }
+}
+
+calcularMedia();
