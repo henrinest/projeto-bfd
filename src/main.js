8c7d4673eebb731334a146c31db2e48c9c024270
eversondamiao <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>
2025-08-26 23:51:09 -0300

    array: prática 2 (array.push e reverse)

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
-const array = [9,18,27,36,45];
-console.log(array)
+const array = [9,18,27,36,45,54,63,72];
+
+array.push(81);
+
+array.push(90);
+
+console.log("array:", array);
+
+const arrayReverted = array.reverse();
+
+console.log("arrayReverted:", arrayReverted);
